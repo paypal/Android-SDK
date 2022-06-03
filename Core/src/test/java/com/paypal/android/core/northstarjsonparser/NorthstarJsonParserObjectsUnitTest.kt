7d0366c9c348ba06bda7b4db789d47e2bb7e9254
Jose Noriega <jonoriega@paypal.com>
2022-06-03 16:32:34 -0300

    Added Complex and Multiparam object to test

--- Core/src/test/java/com/paypal/android/core/northstarjsonparser/NorthstarJsonParserObjectsUnitTest.kt
+++ Core/src/test/java/com/paypal/android/core/northstarjsonparser/NorthstarJsonParserObjectsUnitTest.kt
@@ -102,6 +102,67 @@ class NorthstarJsonParserObjectsUnitTest {
 
         assertEquals(expectedValue, sut)
     }
+
+    @Test
+    fun `given an object with objects, string and list, it should return the same values`() {
+        val json = """
+            {
+                "annotated_param": "value",
+                "list_of_complex_items": [
+                    {
+                        "multi_param_object": {
+                            "param1": "value1",
+                            "param2": "value2",
+                            "param3": "value3",    
+                        }
+                    },
+                    {
+                        "multi_param_object": {
+                            "param1": "value1",
+                            "param2": "value2",
+                            "param3": "value3",    
+                        }
+                    },
+                    {
+                        "multi_param_object": {
+                            "param1": "value1",
+                            "param2": "value2",
+                            "param3": "value3",    
+                        }
+                    },
+                ],
+                "multi_param_object": {
+                    "param1": "value1",
+                    "param2": "value2",
+                    "param3": "value3",    
+                },
+                "list_of_list": [
+                    [
+                        { "value_string": "value" },
+                        { "value_string": "value" },
+                    ],
+                    [
+                        { "value_string": "value" },
+                        { "value_string": "value" },
+                        { "value_string": "value" },
+                    ],
+                    [
+                        { "value_string": "value" },
+                        { "value_string": "value" },
+                        { "value_string": "value" },
+                        { "value_string": "value" },
+                    ],
+                ],
+                "empty_list": [],
+                "simple_object_in_json": {
+                    "value_object": {
+                        "value_string: "another value"
+                    }
+                }
+            }
+        """.trimIndent()
+    }
+
 }
 
 data class StringParamObject(val valueString: String)
@@ -111,3 +172,21 @@ data class SimpleObjectParam(val valueObject: StringParamObject)
 data class ListStringParamObject(val valueList: List<String>)
 
 data class ListOfTypesParamObject(val valueList: List<StringParamObject>)
+
+data class MultiParamObject(
+    val param1: String,
+    val param2: String,
+    val param3: String,
+)
+
+data class ComplexObject(
+    @JsonName("annotated_param") val simpleValue: String,
+    val multiParamObject: MultiParamObject,
+    @JsonName("simple_object_in_json") val simpleObject: SimpleObjectParam,
+    @JsonName("object_not_in_json") val simpleObjectAbsent: SimpleObjectParam? = SimpleObjectParam(
+        StringParamObject("default")
+    ),
+    @JsonName("list_of_complex_items") val listOfItems: List<MultiParamObject>,
+    val listOfList: List<List<StringParamObject>>,
+    val emptyList: List<StringParamObject>
+)
