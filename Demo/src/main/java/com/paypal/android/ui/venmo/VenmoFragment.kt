30dfe2c489574fc331abf99405243a93477c7b68
sshropshire <sshropshire@paypal.com>
2022-06-13 10:32:03 -0500

    Update proof of concept for venmo fragment.

--- Demo/src/main/java/com/paypal/android/ui/venmo/VenmoFragment.kt
+++ Demo/src/main/java/com/paypal/android/ui/venmo/VenmoFragment.kt
@@ -3,26 +3,21 @@ package com.paypal.android.ui.venmo
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
+import com.paypal.android.BuildConfig
 import com.paypal.android.api.model.Amount
-import com.paypal.android.api.model.ApplicationContext
 import com.paypal.android.api.model.CreateOrderRequest
 import com.paypal.android.api.model.Order
 import com.paypal.android.api.model.Payee
 import com.paypal.android.api.model.PurchaseUnit
+import com.paypal.android.api.services.AuthApi
 import com.paypal.android.api.services.PayPalDemoApi
-import com.paypal.android.card.Card
-import com.paypal.android.card.CardRequest
-import com.paypal.android.card.threedsecure.SCA
-import com.paypal.android.card.threedsecure.ThreeDSecureRequest
+import com.paypal.android.core.CoreConfig
 import com.paypal.android.databinding.FragmentVenmoBinding
-import com.paypal.android.ui.card.CardFragment
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -33,7 +28,18 @@ class VenmoFragment : Fragment() {
     @Inject
     lateinit var payPalDemoApi: PayPalDemoApi
 
+    @Inject
+    lateinit var authApi: AuthApi
+
+    private val config = CoreConfig(BuildConfig.CLIENT_ID, BuildConfig.CLIENT_SECRET)
+
     private lateinit var binding: FragmentVenmoBinding
+
+    var progressIndicatorVisible: Boolean
+        get() = (binding.progressIndicator.visibility == View.VISIBLE)
+        set(isVisible) {
+            binding.progressIndicator.visibility = if (isVisible) View.VISIBLE else View.INVISIBLE
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -52,30 +58,27 @@ class VenmoFragment : Fragment() {
     }
 
     private suspend fun launchVenmo() {
+        progressIndicatorVisible = true
 
+        val facilitatorAccessToken = authApi.postOAuthToken().accessToken
         val order = createOrder()
 
+        progressIndicatorVisible = false
         val uri = Uri.parse("https://www.paypal.com/smart/checkout/venmo")
             .buildUpon()
-            .appendQueryParameter("channel", "mobile-web")
-            .appendQueryParameter("sessionUID", "NOOP")
             .appendQueryParameter("orderID", order.id!!)
-            .appendQueryParameter(
-                "facilitatorAccessToken",
-                "A21AANJT-DPnNpO6ZGzrjUH7k__21PSBydkdheCtnuCcL6QVD5cUvr_GUr5anpNkEsDo6pRg9wzc3MRNqhK69xJ9qy7YLxerQ"
-            )
-            .appendQueryParameter(
-                "clientID",
-                "AdLzRW18VHoABXiBhpX2gf0qhXwiW4MmFVHL69V90vciCg_iBLGyJhlf7EuWtFcdNjGiDfrwe7rmhvMZ"
-            )
-            .appendQueryParameter("commit", "false")
-            .appendQueryParameter("webCheckoutUrl", "")
-            .appendQueryParameter("buttonSessionID", "NOOP")
-            .appendQueryParameter("env", "sandbox")
-            .appendQueryParameter("fundingSource", "venmo")
-            .appendQueryParameter("enableFunding", "venmo")
             .appendQueryParameter("buyerCountry", "US")
-            .appendQueryParameter("pageUrl", "www.google.com")
+            .appendQueryParameter("fundingSource", "venmo")
+            .appendQueryParameter("sessionUID", "NOOP")
+//            .appendQueryParameter("buttonSessionID", "NOOP")
+            .appendQueryParameter("channel", "")
+            .appendQueryParameter("enableFunding", "venmo")
+            .appendQueryParameter("clientID", config.clientId)
+            .appendQueryParameter("facilitatorAccessToken", facilitatorAccessToken)
+            .appendQueryParameter("commit", "false")
+//            .appendQueryParameter("webCheckoutUrl", "")
+//            .appendQueryParameter("env", "Sandbox")
+            .appendQueryParameter("pageUrl", "com.paypal.android.demo://return_url")
             .appendQueryParameter("sdkVersion", "5.0.304")
             .build()
 
